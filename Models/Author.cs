dc4b1fcf6cf86743465a4696dc1c6ed567f11b3e
Nate Carlin <carlin010@gannon.edu>
2023-09-17 22:15:31 -0400

    Minor changes for HW1 template

---
+++ Models/Author.cs
@@ -0,0 +1,6 @@
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Nationality { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -9,13 +9,12 @@ builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1"});
 });
 
-// This line registers the BookService class with the application's "services container."
-// The 'AddSingleton' method tells the application to create one instance of BookService and
+// This line registers the IBookService class with the application's "services container."
+// The 'AddSingleton' method tells the application to create one instance of IBookService and
 // use that same instance every time the service is needed. This way, you don't have to manually
-// create a new BookService each time you need to use it; the system will automatically give you
+// create a new IBookService each time you need to use it; the system will automatically give you
 // the one it has created. This is a technique called "Dependency Injection."
-builder.Services.AddSingleton<BookService>();
-
+builder.Services.AddSingleton<IBookService, BookService>();
 
 var app = builder.Build();
 
@@ -31,14 +30,14 @@ app.MapGet("/", () => "Hello World!");
 
 // Mapping an HTTP GET request to the "/books" endpoint.
 // When someone navigates to this URL, the list of all books will be returned.
-app.MapGet("/books", (BookService bookService) => {
+app.MapGet("/books", (IBookService bookService) => {
     return bookService.GetAllBooks();
 });
 
 // Mapping an HTTP GET request to the "/books/{id}" endpoint.
 // The {id} in the URL is a parameter, meaning it will be replaced by the ID of the book you're looking to get.
 // For example, "/books/1" will look for the book with an ID of 1.
-app.MapGet("/books/{id}", (int id, BookService bookService) => 
+app.MapGet("/books/{id}", (int id, IBookService bookService) => 
 {
     // Use LINQ's FirstOrDefault method to search for a book with a matching ID in the list.
     // If no book is found, 'book' will be null.
@@ -54,9 +53,9 @@ app.MapGet("/books/{id}", (int id, BookService bookService) =>
 // Map a POST request to the "/books" endpoint.
 // This will allow clients to create new books.
 // The new book's data will be passed in the request body and mapped to the 'newBook' parameter.
-app.MapPost("/books", (Book newBook, BookService bookService) =>
+app.MapPost("/books", (Book newBook, IBookService bookService) =>
 {
-    // Add the new book to the list of books in the BookService class.
+    // Add the new book to the list of books in the IBookService class.
     bookService.AddBook(newBook);
 
     // Return a 201 Created status along with the newly created book.
@@ -67,7 +66,7 @@ app.MapPost("/books", (Book newBook, BookService bookService) =>
 // Map a PUT request to the "/books/{id}" endpoint.
 // This will allow clients to update an existing book identified by its ID.
 // The book's new data will be passed in the request body and mapped to the 'updatedBook' parameter.
-app.MapPut("/books/{id}", (int id, Book updatedBook, BookService bookService) =>
+app.MapPut("/books/{id}", (int id, Book updatedBook, IBookService bookService) =>
 {
     bookService.UpdateBook(updatedBook);
 
@@ -78,7 +77,7 @@ app.MapPut("/books/{id}", (int id, Book updatedBook, BookService bookService) =>
 
 // Map a DELETE request to the "/books/{id}" endpoint.
 // This will allow clients to delete an existing book identified by its ID.
-app.MapDelete("/books/{id}", (int id, BookService bookService) =>
+app.MapDelete("/books/{id}", (int id, IBookService bookService) =>
 {
     bookService.DeleteBook(id);
 
